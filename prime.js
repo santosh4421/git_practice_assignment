9c970b172f1f6fbf08c8fae0f9a237c327b4aaa0
santosh4421 <santoshdaberao16111110@gmail.com>
2022-11-03 18:02:30 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,18 @@
+let factors=0;
+let number=6
+for(let i=0;i<=number;i++)
+{
+  if(i%number==0)
+{
+  factor++;
+}
+}
+if(facotrs==5)
+{
+  console.log("prime")
+}
+else
+{
+  console.log("not prime")
+}
+  
