3e5c7dc7aa0970e3f7de149491cc68e71317a622
ajtayde001 <ajtayde001@gmail.com>
2022-11-03 21:26:14 +0530

    code fixed by ajay

--- prime.js
+++ prime.js
@@ -1,18 +1,21 @@
+function prime(number){
 let factors=0;
-let number=6
 for(let i=0;i<=number;i++)
 {
-  if(i%number==0)
+  if(number%i==0)
 {
   factor++;
 }
 }
-if(facotrs==5)
-{
-  console.log("prime")
+if(facotrs==2){
+   return true;
+     }
+      return false;
 }
-else
-{
-  console.log("not prime")
+
+let answer=prime(13);
+if(answer==true){
+   console.log("prime");
 }
-  
+else{
+    console.log("Not prime"); 
